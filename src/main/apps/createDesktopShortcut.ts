281fa158974f6fb8cc38a8f08ab2584c1d3d348f
Mia Rosie Lleorenig <Mia.Lleorenig@nordicsemi.no>
2026-04-09 12:13:46 +0200

    fix: wrap linux shortcut executable path in double quotes
    This crashes on ubuntu 24.04 otherwise

--- src/main/apps/createDesktopShortcut.ts
+++ src/main/apps/createDesktopShortcut.ts
@@ -113,7 +113,7 @@ const createShortcutForLinux = (app: LaunchableApp) => {
         'Encoding=UTF-8',
         `Version=${app.currentVersion}`,
         `Name=${fileName}`,
-        `Exec=${getElectronExePath().replace(/ /g, '\\ ')} ${args}`,
+        `Exec="${getElectronExePath()}" ${args}`,
         'Terminal=false',
         `Icon=${icon}`,
         'Type=Application',
